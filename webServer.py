79633b44b610f9187b61c53268fe1a218f61b311
jcs9971 <112672563+jcs9971@users.noreply.github.com>
2023-02-15 12:31:57 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -36,14 +36,14 @@ def webServer(port=13331):
 
             # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
             # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
+            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
 
 
             # Send the content of the requested file to the client
             for i in f:  # for line in file
             # Fill in start - send your html file contents #Fill in end
                 connectionSocket.send(outputdata[i])
-            connectionSocket.send("\r\n")
+            connectionSocket.send("\r\n".encode())
                 
             connectionSocket.close()
                      # fill in end
