16c28c83a33852edd30231624dbda72d684738b3
jcs9971 <112672563+jcs9971@users.noreply.github.com>
2023-02-15 15:30:28 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -3,7 +3,6 @@ from socket import *
 # In order to terminate the program
 import sys
 
-
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -18,47 +17,57 @@ def webServer(port=13331):
         # Establish the connection
 
         print('The server is ready to receive')
-        connectionSocket, addr =  serverSocket.accept() # Fill in start -are you accepting connections?     #Fill in end
+        connectionSocket, addr = serverSocket.accept()  # Fill in start -are you accepting connections?     #Fill in end
 
         try:
-            message =  connectionSocket.recv(1024).decode() #Fill in start -a client is sending you a message   #Fill in end
+            message = connectionSocket.recv(1024).decode()  # Fill in start -a client is sending you a message
+
+            print(message)
+
+            # #Fill in end
             filename = message.split()[1]
 
+            print(filename)
 
             # opens the client requested file.
-            # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-            f = open(filename[1:], "b")  
-            
+            # Plenty of guidance online on how to open and read a file in python. How should you read it though
+            # if you plan on sending it through a socket?
+            f = open(filename[1:], "b")
+
             outputdata = f.readlines()
             #print(outputdata)
 
-            #outputdata=b"Content-Type: text/html; charset=UTF-8\r\n"
+            # outputdata=b"Content-Type: text/html; charset=UTF-8\r\n"
 
-            # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
-            # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-
+            # Send an HTTP header line into socket for a valid request. What header should be sent for a response
+            # that is ok?
+            # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n"
+            # Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
+            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
 
             # Send the content of the requested file to the client
             for i in f:  # for line in file
-            # Fill in start - send your html file contents #Fill in end
+                # Fill in start - send your html file contents #Fill in end
                 connectionSocket.send(outputdata[i])
             connectionSocket.send("\r\n".encode())
-                
+
             connectionSocket.close()
-                     # fill in end
+            # fill in end
 
 
         except Exception as e:
             connectionSocket.send("\nHTTP/1.1 404 Not Found\n\n".encode())
             connectionSocket.close()
+
+    print(e)
+
     # Send response message for invalid request due to the file not being found (404)
     # Fill in start
 
-    # Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
+    # Commenting out the below, as its technically not required and some students have moved it erroneously in the
+    # While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
     # serverSocket.close()
     # sys.exit()  # Terminate the program after sending the corresponding data
-
 
 if __name__ == "__main__":
     webServer(13331)
